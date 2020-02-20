10d16de6cc90a670c6fe82b5bf962408d19d2979
23369 <123456>
2020-02-20 22:29:37 +0800

    测试Lombok组件能否使用

---
+++ test/test/LombokTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @program: hern-shop
+ * @description: 测试Lombok组件
+ * @author: 宋兆恒-2336909208@q.com
+ * @create: 2020-02-20 22:24
+ **/
+@Getter
+@Setter
+public class LombokTest {
+    private String name;
+
+}
---
+++ test/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * @program: hern-shop
+ * @description: 测试Test
+ * @author: 宋兆恒-2336909208@q.com
+ * @create: 2020-02-20 22:26
+ **/
+public class Test {
+    public static void main(String[] args) {
+        LombokTest lombokTest = new LombokTest();
+        lombokTest.setName("Hern");
+        System.out.println(lombokTest.getName());
+    }
+}

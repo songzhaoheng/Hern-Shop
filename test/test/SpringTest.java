73abf7f581227f176eca57f42dbbf811c1225ae3
23369 <123456>
2020-02-20 22:24:07 +0800

    添加Lombok自动注解组件到pom.xml中

--- test/test/SpringTest.java
+++ test/test/SpringTest.java
@@ -1,10 +1,14 @@
 package test;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @program: hern-shop
  * @description: spring组件测试
  * @author: 宋兆恒-2336909208@q.com
  * @create: 2020-02-20 22:18
  **/
+@Controller
 public class SpringTest {
+    private String id;
 }
